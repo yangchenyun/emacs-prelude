a1019965ca76c9ec1619a6c35ba0281128319a64
Steven Yang <yangchenyun@gmail.com>
2014-05-15 00:59:25 +0800

    config org-mode to usable state - todo tags and coloring finished - basic file storage finished - keymap shortcuts finished - helper functions borrowed somewhere - org-protocol set up

--- setup-org.el
+++ setup-org.el
@@ -1,24 +1,6 @@
-(prelude-require-package 'org-mac-link)
 (require 'org-protocol)
 ;; Register a org-protocol:// scheme onto the system
 ;; http://jcardente.blogspot.com/2010/09/saving-weblinks-to-org-mode-from-safari.html
-
-;; adapted from evil-org-mode with customized settings for evil keymaps
-(define-minor-mode evil-org-mode
-  "Buffer local minor mode for evil-org"
-  :init-value nil
-  :lighter " EvilOrg"
-  :keymap (make-sparse-keymap) ; defines evil-org-mode-map
-  :group 'evil-org)
-
-(add-hook 'org-mode-hook 'evil-org-mode)
-
-(defun always-insert-item ()
-  "Force insertion of org item"
-  (if (not (org-in-item-p))
-      (insert "\n- ")
-    (org-insert-item))
-  )
 
 (defun evil-org-eol-call (fun)
   "Go to end of line and call provided function"
@@ -28,7 +10,7 @@
   )
 
 ;; normal state shortcuts
-(evil-define-key 'normal evil-org-mode-map
+(evil-define-key 'normal org-mode-map
   "gu" 'outline-up-heading
   "gj" 'org-forward-heading-same-level
   "gk" 'org-backward-heading-same-level
@@ -40,17 +22,17 @@
   "t" 'org-todo
   "T" 'org-set-tags-command
   ";t" 'org-show-todo-tree
-  "O" '(lambda () (interactive) (evil-org-eol-call 'org-insert-heading))
   "$" 'org-end-of-line
   "^" 'org-beginning-of-line
   "<" 'org-metaleft
   ">" 'org-metaright
   "-" 'org-cycle-list-bullet
+  "W" 'org-refile
   (kbd "TAB") 'org-cycle)
 
 ;; normal & insert state shortcuts.
 (mapc (lambda (state)
-        (evil-define-key state evil-org-mode-map
+        (evil-define-key state org-mode-map
           (kbd "M-l") 'org-metaright
           (kbd "M-h") 'org-metaleft
           (kbd "M-k") 'org-metaup
@@ -81,28 +63,240 @@
   "cq" 'org-clock-cancel
   "cd" 'org-clock-display
   "cj" 'org-clock-goto
-  "ce" 'org-clock-modify-effort-estimate
+  "cr" 'org-clock-report
+
+  "e" 'org-clock-modify-effort-estimate
+  "a" 'org-archive-subtree-default
 )
 
-(defun config-org-mode ()
+
+(setq org-directory (expand-file-name "~/Dropbox/.org")
+      org-agenda-files (list org-directory)
+      org-default-notes-file (expand-file-name "inbox.org" org-directory)
+      org-completion-use-ido t        ; Complete with IDO in Org
+      org-yank-adjusted-subtrees t    ; Adjust level when yanking entire trees
+      org-enforce-todo-dependencies t
+      org-clock-into-drawer t
+      org-M-RET-may-split-line nil
+      org-refile-targets '((org-agenda-files :maxlevel . 4))
+      org-archive-default-command 'org-archive-subtree-default-with-confirmation
+      org-clock-persist 'history
+      org-todo-keywords '((sequence
+                           "TODO(t)"
+                           "STARTED(s)"
+                           "WAITING(w@/!)" ;; note for enter, timestamp for leaving
+                           "SOMEDAY(.)"
+                           "|" "DONE(x!)" "CANCELLED(c@)")
+                          (sequence "TODELEGATE(-@)" "DELEGATED(d!)" "COMPLETE(x!)"))
+      org-todo-keyword-faces
+      '(("TODO" . (:foreground "#eee8d5" :weight bold))
+        ("TODELEGATE" . (:foreground "#eee8d5" :weight bold))
+        ("STARTED" . (:foreground "#dc322f" :weight bold))
+        ("WAITING" . (:foreground "#9EA0E5" :weight bold))
+        ("DELEGATED" . (:foreground "#9EA0E5" :weight bold))
+        ("SOMEDAY" . (:foreground "#F771AC" :weight bold))
+
+        ("CANCELLED" . (:foreground "#839496" :weight normal))
+        ("COMPLETE" . (:foreground "#839496" :weight normal :strike-through t))
+        ("DONE" . (:foreground "#B4C342" :weight normal :strike-through t))
+        )
+      org-fontify-done-headline t
+
+      org-structure-template-alist
+      '(("s" "#+begin_src ?\n\n#+end_src" "<src lang=\"?\">\n\n</src>")
+        ("e" "#+begin_example\n?\n#+end_example" "<example>\n?\n</example>")
+        ("q" "#+begin_quote\n?\n#+end_quote" "<quote>\n?\n</quote>")
+        ("v" "#+BEGIN_VERSE\n?\n#+END_VERSE" "<verse>\n?\n</verse>")
+        ("c" "#+BEGIN_COMMENT\n?\n#+END_COMMENT")
+        ("l" "#+begin_src emacs-lisp\n?\n#+end_src" "<src lang=\"emacs-lisp\">\n?\n</src>")
+        ("L" "#+latex: " "<literal style=\"latex\">?</literal>")
+        ("h" "#+begin_html\n?\n#+end_html" "<literal style=\"html\">\n?\n</literal>")
+        ("H" "#+html: " "<literal style=\"html\">?</literal>")
+        ("a" "#+begin_ascii\n?\n#+end_ascii")
+        ("A" "#+ascii: ")
+        ("i" "#+index: ?" "#+index: ?")
+        ("I" "#+include %file ?" "<include file=%file markup=\"?\">"))
+      )
+
+;; strike through headline
+(custom-set-faces
+ '(org-headline-done
+   ((((class color) (min-colors 16) (background dark))
+     (:foreground "#839496" :strike-through t)))))
+
+(defvar sacha/org-basic-task-template "* TODO %^{Task}
+SCHEDULED: %^t
+%?
+:PROPERTIES:
+:Effort: %^{effort|1:00|0:05|0:15|0:30|2:00|4:00}
+:END:" "Basic task data")
+
+(setq org-capture-templates
+      `(
+        ("t" "Tasks" entry
+         (file+headline (expand-file-name "inbox.org" org-directory) "Tasks")
+         ,sacha/org-basic-task-template)
+        ;; for insert tasks quickly
+        ("dt" "Done - Task" entry
+         (file+headline (expand-file-name "inbox.org" org-directory) "Tasks")
+         "* DONE %^{Task}\nSCHEDULED: %^t\n%?")
+        ("i" "Ideas" entry
+         (file+headline (expand-file-name "inbox.org" org-directory) "Ideas")
+         "* %^{Idea} %u\n%?\n\n%i")
+        ("q" "Quick note" item
+         (file+headline (expand-file-name "inbox.org" org-directory) "Quick Notes"))
+        ("j" "Daily Journal" entry
+         (file+datetree (expand-file-name "inbox.org" org-directory))
+         "* %?\n\n%i\n")))
+
+(defun org-dblock-write:rangereport (params)
+  "Display day-by-day time reports."
+  (let* ((ts (plist-get params :tstart))
+         (te (plist-get params :tend))
+         (start (time-to-seconds
+                 (apply 'encode-time (org-parse-time-string ts))))
+         (end (time-to-seconds
+               (apply 'encode-time (org-parse-time-string te))))
+         day-numbers)
+    (setq params (plist-put params :tstart nil))
+    (setq params (plist-put params :end nil))
+    (while (<= start end)
+      (save-excursion
+        (insert "\n\n"
+                (format-time-string (car org-time-stamp-formats)
+                                    (seconds-to-time start))
+                "----------------\n")
+        (org-dblock-write:clocktable
+         (plist-put
+          (plist-put
+           params
+           :tstart
+           (format-time-string (car org-time-stamp-formats)
+                               (seconds-to-time start)))
+          :tend
+          (format-time-string (car org-time-stamp-formats)
+                              (seconds-to-time end))))
+        (setq start (+ 86400 start))))))
+
+;; FIXME, the org-make-tags-matcher reports error
+(defun wicked/org-calculate-tag-time (matcher &optional ts te)
+  "Return the total minutes clocked in headlines matching MATCHER.
+MATCHER is a string or a Lisp form to be evaluated, testing if a
+given set of tags qualifies a headline for inclusion. TS and TE
+are time start (inclusive) and time end (exclusive). Call with a
+prefix to be prompted for TS and TE.
+
+For example, to see how much time you spent on tasks tagged as
+URGENT, call M-x wicked/org-calculate-tag-time RET URGENT RET. To
+see how much time you spent on tasks tagged as URGENT today, call
+C-u M-x wicked/org-calculate-tag-time RET URGENT RET . RET +1 RET."
+  (interactive (list
+		(read-string "Tag query: ")
+		(if current-prefix-arg (org-read-date))
+		(if current-prefix-arg (org-read-date))))
+  ;; Convert strings to proper arguments
+  (if (stringp matcher) (setq matcher (cdr (org-make-tags-matcher matcher))))
+  (if (stringp ts)
+      (setq ts (time-to-seconds (apply 'encode-time (org-parse-time-string ts)))))
+  (if (stringp te)
+      (setq te (time-to-seconds (apply 'encode-time (org-parse-time-string te)))))
+  (let* ((re (concat "[\n\r]" outline-regexp " *\\(\\<\\("
+		     (mapconcat 'regexp-quote org-todo-keywords-1 "\\|")
+		     (org-re
+		      "\\>\\)\\)? *\\(.*?\\)\\(:[[:alnum:]_@:]+:\\)?[ \t]*$")))
+	 (case-fold-search nil)
+         lspos
+	 tags tags-list tags-alist (llast 0) rtn level category i txt p
+	 marker entry priority (total 0))
+    (save-excursion
+      (org-clock-sum ts te)
+      (goto-char (point-min))
+      (while (re-search-forward re nil t)
+	(catch :skip
+	  (setq tags (if (match-end 4) (match-string 4)))
+	  (goto-char (setq lspos (1+ (match-beginning 0))))
+	  (setq level (org-reduced-level (funcall outline-level))
+		category (org-get-category))
+	  (setq i llast llast level)
+	  ;; remove tag lists from same and sublevels
+	  (while (>= i level)
+	    (when (setq entry (assoc i tags-alist))
+	      (setq tags-alist (delete entry tags-alist)))
+	    (setq i (1- i)))
+	  ;; add the nex tags
+	  (when tags
+	    (setq tags (mapcar 'downcase (org-split-string tags ":"))
+		  tags-alist
+		  (cons (cons level tags) tags-alist)))
+	  ;; compile tags for current headline
+	  (setq tags-list
+		(if org-use-tag-inheritance
+		    (apply 'append (mapcar 'cdr tags-alist))
+		  tags))
+	  (when (and (eval matcher)
+		     (or (not org-agenda-skip-archived-trees)
+			 (not (member org-archive-tag tags-list))))
+	    ;; Get the time for the headline at point
+	    (goto-char (line-beginning-position))
+	    (setq total (+ total (or (get-text-property (1+ (point)) :org-clock-minutes) 0)))
+	    ;; if we are to skip sublevels, jump to end of subtree
+	    (org-end-of-subtree t)))))
+    (if (called-interactively-p `interactive)
+	(let* ((h (/ total 60))
+	       (m (- total (* 60 h))))
+	  (message "Time: %d:%02d (%d hours and %d minutes)" h m h m)))
+    total))
+
+(defun sacha/org-agenda-done (&optional arg)
+  "Mark current TODO as done.
+This changes the line at point, all other lines in the agenda referring to
+the same tree node, and the headline of the tree node in the Org-mode file."
+  (interactive "P")
+  (org-agenda-todo "DONE"))
+(defun sacha/org-agenda-mark-done-and-add-followup ()
+  "Mark the current TODO as done and add another task after it.
+Creates it at the same level as the previous task, so it's better to use
+this with to-do items than with projects or headings."
   (interactive)
-  (setq org-todo-keyword-faces
-        '(("TODO" . org-warning)
-          ("STARTED" . "yellow")
-          ("CANCELED" . (:foreground "blue" :weight bold))))
-  (setq org-enforce-todo-dependencies t)
-  (setq org-M-RET-may-split-line nil)
-  (add-to-list 'org-modules "org-habit")
-  (org-agenda-files "~/.org")
-  (org-indent-mode t)
-  (lambda ()
-    (local-unset-key (kbd "C-u")))
-)
+  (org-agenda-todo "DONE")
+  (org-agenda-switch-to)
+  (org-capture 0 "t"))
 
-;; only load with org-mode is activated
-(add-hook 'org-mode-hook 'config-org-mode)
+(defun sacha/org-agenda-new ()
+  "Create a new note or task at the current agenda item.
+Creates it at the same level as the previous task, so it's better to use
+this with to-do items than with projects or headings."
+  (interactive)
+  (org-agenda-switch-to)
+  (org-capture 0))
 
-(setq org-clock-persist 'history)
-(org-clock-persistence-insinuate)
+(defun sacha/org-agenda-list-unscheduled (&rest ignore)
+  "Create agenda view for tasks that are unscheduled and not done."
+  (let* ((org-agenda-todo-ignore-with-date t)
+   (org-agenda-overriding-header "List of unscheduled tasks: "))
+    (org-agenda-get-todos)))
+(setq org-stuck-projects
+      '("+PROJECT-MAYBE-DONE"
+        ("TODO")
+        nil
+        "\\<IGNORE\\>"))
+
+;; Override the key definition in org-agenda
+(org-add-hook
+ 'org-agenda-mode-hook
+ '(lambda ()
+   (define-key org-agenda-mode-map "x" 'sacha/org-agenda-done)
+   (define-key org-agenda-mode-map "X" 'sacha/org-agenda-mark-done-and-add-followup)
+   (define-key org-agenda-mode-map "N" 'sacha/org-agenda-new)))
+
+(org-add-hook
+ 'org-mode-hook
+ '(lambda ()
+    (org-indent-mode)
+    (org-clock-persistence-insinuate)
+    (add-to-list 'org-modules "org-habit")))
+
+;; disable longline highlighting
+;; (add-hook 'org-mode-hook #'stante-whitespace-style-no-long-lines)
 
 (provide 'setup-org)
