2dbd4f2ad0a0ac26adacbf206e17e65897d2c363
Steven Yang <yangchenyun@gmail.com>
2014-05-28 22:32:01 +0800

    use hooks for cider evil keymap setup

--- setup-cider.el
+++ setup-cider.el
@@ -1,10 +1,10 @@
 (eval-after-load 'cider-mode
-  '(fill-keymaps '(cider-mode-map
-                   evil-normal-state-map)
-     "M-." 'cider-jump
-     "M-," 'cider-jump-back
-     "C-<RET>"     'cider-eval-last-sexp
-     "M-<RET>"     'cider-eval-defun-at-point))
+  '(progn
+     (defvar cider-mode-map)
+     (evil-make-overriding-map cider-mode-map 'normal)
+     (evil-define-key 'normal cider-mode-map
+       (kbd "M-<RET>") 'cider-eval-last-sexp
+       (kbd "M-S-<RET>") 'cider-eval-defun-at-point)))
 
 (provide 'setup-cider)
 ;;; setup-cider.el ends here
