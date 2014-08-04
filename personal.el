1491869660cc1f0e083427adec015912565fd661
Steven Yang <yangchenyun@gmail.com>
2014-08-05 03:33:43 +0800

    removed exec-path hacks as it is fixed in release

--- personal.el
+++ personal.el
@@ -60,16 +60,6 @@
 ; be quiet
 (setq ring-bell-function 'ignore)
 
-;;; ISSUE fish Shell separates PATH with whitespace instead of :
-;;; https://github.com/purcell/exec-path-from-shell/issues/9
-(when (eq system-type 'darwin)
-  (exec-path-from-shell-initialize)
-  (when (equal (file-name-nondirectory (getenv "SHELL")) "fish")
-    (setq exec-path (split-string (car exec-path) " "))
-    (let ((fixed-path (mapconcat 'identity (split-string (getenv "PATH") " ") ":")))
-         (setenv "PATH" fixed-path)
-         (setq eshell-path-env fixed-path))))
-
 (setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
 (setq ns-function-modifier 'hyper)
