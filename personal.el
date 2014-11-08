93f11dd4767725896fd79c590a9ab5f6a16dcdf8
Steven Yang <yangchenyun@gmail.com>
2014-11-07 16:50:24 -0800

    configure prelude modules in personal.el and remove redundant configurations

--- personal.el
+++ personal.el
@@ -1,3 +1,39 @@
+;; Emacs IRC client
+(require 'prelude-erc)
+(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
+(require 'prelude-helm) ;; Interface for narrowing and search
+;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
+(require 'prelude-company)
+;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
+;; (require 'prelude-mediawiki)
+;; (require 'prelude-evil)
+
+;;; Programming languages support
+(require 'prelude-c)
+;; (require 'prelude-clojure)
+;; (require 'prelude-coffee)
+;; (require 'prelude-common-lisp)
+;; (require 'prelude-css)
+(require 'prelude-emacs-lisp)
+;; (require 'prelude-erlang)
+;; (require 'prelude-go)
+;; (require 'prelude-haskell)
+(require 'prelude-js)
+;; (require 'prelude-latex)
+(require 'prelude-lisp)
+;; (require 'prelude-ocaml)
+(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
+(require 'prelude-perl)
+(require 'prelude-python)
+(require 'prelude-ruby)
+;; (require 'prelude-scala)
+(require 'prelude-scheme)
+(require 'prelude-shell)
+;; (require 'prelude-scss)
+(require 'prelude-web) ;; Emacs mode for web templates
+(require 'prelude-xml)
+(require 'prelude-yaml)
+
 (prelude-require-package 'ido-vertical-mode)
 (ido-vertical-mode 1)
 
@@ -49,10 +85,6 @@
 (prelude-require-package 'e2wm)
 (require 'e2wm)
 
-(prelude-require-package 'web-mode)
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
