487d7843bc7449b1176240b7f110c2ad7b773215
Steven Yang <yangchenyun@gmail.com>
2015-03-03 11:13:14 -0800

    finished the logged time computation in awesome-plan

--- org-awesome-plan.el
+++ org-awesome-plan.el
@@ -1,4 +1,4 @@
-;;; org-awesome-plan.el --- Make org-mode more awesome with plans.
+;;;  org-awesome-plan.el --- Make org-mode more awesome with plans.
 
 ;;; Commentary:
 
@@ -13,9 +13,8 @@
 ;; Plan has a covering period and available hours, in plain English, I plan to "spend * hours in this period"; currently available period includes yearly / monthly / weekly plan.
 
 ;;; Code:
-;; TODO: reducers to collect by weekly/monthly/yearly of:
-;; 1. logged time
-;; 2. planned time
+;; 1. [done] logged time (needs to compute time range though)
+;; 2. [done] planned time
 ;; then it is possible to answer questions like
 ;; How much effort I've planned on in Jan? For Category 2, for Goal 2 in Jan?
 ;; How much working hours I have commited in Jan? For Category 2? For Goal 2?
@@ -27,6 +26,23 @@
 
 ;; TODO: monthly and weekly plan should be nested into the yearly plan
 
+(require 'dash)
+
+(defvar org-tr-regexp-both)
+
+(declare-function calendar-month-name "calendar" (month &optional abbrev))
+(declare-function org-clock-special-range "org-clock" (key &optional time as-strings wstart mstart))
+(declare-function org-back-to-heading "org" (&optional invisible-ok))
+(declare-function org-at-heading-p "org" (&optional ignored))
+(declare-function org-end-of-subtree "org" (&optional invisible-ok to-heading))
+(declare-function org-float-time "org-compat" (&optional time))
+(declare-function org-time-string-to-time "org" (s &optional buffer pos))
+(declare-function org-make-tdiff-string "org" (y d h m))
+(declare-function org-read-property-value "org" (property))
+(declare-function org-set-property "org" (property value))
+(declare-function org-entry-get "org" (pom property &optional inherit literal-nil))
+(declare-function org-occur "org" (regexp &optional keep-previous callback))
+
 (defun org-awesome-plan/plan-label (plan-type)
   (upcase (format "%s_plan" plan-type)))
 
@@ -34,7 +50,77 @@
   (concat ":" (org-awesome-plan/plan-label plan-type) ":[ \t]+"
           "\\((\\(([[:digit:] \\t\\.]+)[ \\t]*\\)+)\\)"))
 
+;; TODO: add a helper method to calculate a TIME based on
+;; week / month / year and passed to org-clock-special-range
+(defun org-awesome-plan/logged-hours-for-week ()
+  "Calculate the sum of logged hours for current subtree of the
+WEEK of current year."
+  (interactive)
+  (org-awesome-plan/logged-hours-for-period
+   (butlast (org-clock-special-range 'thisweek))))
+
+(defun org-awesome-plan/logged-hours-for-month ()
+  "Calculate the sum of logged hours for current subtree of the
+MONTH of current year."
+  (interactive)
+  (org-awesome-plan/logged-hours-for-period
+   (butlast (org-clock-special-range 'thismonth))))
+
+(defun org-awesome-plan/logged-hours-for-year ()
+  "Calculate the sum of logged hours for current subtree in the
+YEAR."
+  (interactive)
+  (org-awesome-plan/logged-hours-for-period
+   (butlast (org-clock-special-range 'thisyear))))
+
+(defun org-awesome-plan/logged-hours-for-period (range)
+  "Calculate the sum of logged hours for current subtree in the
+RANGE of time."
+  (let* ((begin-time (car range))
+         (end-time (cadr range))
+         (beg (save-excursion
+                (org-back-to-heading t)
+                (point)))
+         (end (save-excursion
+                (org-end-of-subtree t t)
+                (if (and (org-at-heading-p) (not (eobp)))
+                    (backward-char 1))
+                (point)))
+         (diffs '()))
+    (save-excursion
+      (save-match-data
+        (goto-char beg)
+        (while (re-search-forward org-tr-regexp-both end t)
+            ;; reference `org-evaluate-time-range' for diff calculate
+            ;; Remove the cases for time string without time and ignore
+            ;; calculation for years.
+          (let* ((ts1 (match-string 1))
+                 (ts2 (match-string 2))
+                 (time1 (org-time-string-to-time ts1))
+                 (time2 (org-time-string-to-time ts2))
+                 (t1 (max (org-float-time time1)
+                          (org-float-time begin-time)))
+                 (t2 (min (org-float-time time2)
+                          (org-float-time end-time)))
+                 (diff (- t2 t1)))
+            ;; ignore cases when there is no overlapping of period and
+            ;; logging time ranges
+            (when (> diff 0)
+              (push diff diffs))))
+        (let* ((diff (-reduce '+ diffs))
+               (ds (* 24 60 60))
+               (hs (* 60 60))
+               (fh "%02d:%02d")
+               (fd (concat "%dd" fh))
+               d h m)
+          (setq
+           d (floor (/ diff ds))  diff (mod diff ds)
+           h (floor (/ diff hs))  diff (mod diff hs)
+           m (floor (/ diff 60)))
+          (message "%s" (org-make-tdiff-string 0 d h m)))))))
+
 (defun org-awesome-plan/mark-as-goal ()
+  (interactive)
   (let ((value (org-read-property-value "GOAL_ID")))
     (org-set-property "GOAL_ID" value)))
 
@@ -42,6 +128,7 @@
   (org-entry-get pom "GOAL_ID" nil))
 
 (defun org-awesome-plan/mark-as-category ()
+  (interactive)
   (let ((value (org-read-property-value "CATEGORY_ID")))
     (org-set-property "CATEGORY_ID" value)))
 
@@ -50,7 +137,7 @@
 
 (defun org-awesome-plan/only-show-plan-for-week (week)
   (org-awesome-plan/interactive-for week)
-    (org-awesome-plan/only-show-plan 'week week))
+  (org-awesome-plan/only-show-plan 'week week))
 
 (defun org-awesome-plan/only-show-plan-for-month (month)
   (org-awesome-plan/interactive-for month)
@@ -58,7 +145,7 @@
 
 (defun org-awesome-plan/only-show-plan-for-year (year)
   (org-awesome-plan/interactive-for year)
-    (org-awesome-plan/only-show-plan 'year year))
+  (org-awesome-plan/only-show-plan 'year year))
 
 (defun org-awesome-plan/total-hours-for-year (year)
   (org-awesome-plan/interactive-for year)
@@ -100,7 +187,7 @@ call. "
            (cur-month (nth 4 (decode-time (current-time))))
            (cur-year (nth 5 (decode-time (current-time))))
            (hours ,(when (member 'hours arg-types)
-                    '(read-number "Hours: ")))
+                     '(read-number "Hours: ")))
            (week ,(when (member 'week arg-types)
                     '(read-number "Week: " cur-week)))
            (month ,(when (member 'month arg-types)
@@ -140,8 +227,8 @@ call. "
 
     (save-match-data
       (message "%d goals in %d %s."
-              (org-occur regexp nil callback)
-              period plan-type))))
+               (org-occur regexp nil callback)
+               period plan-type))))
 
 (defun org-awesome-plan/update-plans-property (plan-type period hours)
   "Update the PROPERTY for hours allocated in the PERIOD of
@@ -149,11 +236,11 @@ PLAN-TYPE. PLAN-TYPE should be one of 'week/'month/'year and
 formatted with `org-awesome-plan/plan-label'. "
   (let ((label (org-awesome-plan/plan-label plan-type)))
     (org-set-property
-    label
-    (format "%s" (org-awesome-plan/create-or-update-plans
-                  period hours
-                  (car (read-from-string
-                        (or (org-entry-get nil label nil) "nil"))))))))
+     label
+     (format "%s" (org-awesome-plan/create-or-update-plans
+                   period hours
+                   (car (read-from-string
+                         (or (org-entry-get nil label nil) "nil"))))))))
 
 ;; the data model for plan
 (defun org-awesome-plan/create-or-update-plans (period hours &optional plans)
@@ -184,3 +271,4 @@ return a list contain a new plan created by
   (cdr plan))
 
 (provide 'org-awesome-plan)
+;;; org-awesome-plan.el ends here
