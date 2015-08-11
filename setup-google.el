15a4f7d8959a1e4628caab8aa329c939e0dff949
Steven Yang <yangchenyun@gmail.com>
2015-08-11 14:15:43 -0700

    gmacs config

--- setup-google.el
+++ setup-google.el
@@ -7,6 +7,12 @@
 
 ;;; Code:
 (unless (not (file-exists-p "/google"))
+  (setq ami-emacs-version
+        (concat "gmacs-" (replace-regexp-in-string "\\.[0-9]+$" ""
+                                                   emacs-version)))
+  (setq debian-emacs-flavor (make-symbol ami-emacs-version))
+  (add-to-list 'load-path (concat "/usr/share/" ami-emacs-version
+                                  "/site-lisp/google/"))
   (require 'google))
 
 (provide 'setup-google)
