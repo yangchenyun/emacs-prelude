848eae6f9b36fe905861ffbcf031bafb1f80b015
Steven Yang <yangchenyun@gmail.com>
2015-12-08 11:48:05 -0800

    add ycmd google setup

--- setup-google.el
+++ setup-google.el
@@ -8,15 +8,17 @@
 ;;; Code:
 (unless (not (file-exists-p "/google"))
   ;; Add gmacs's site-lisp load-path
-  ;; NOTE: in order to use ELPA's version of third_party packages, 
+  ;; NOTE: in order to use ELPA's version of third_party packages,
   ;;   some packages are commented out in the google.el loaded below
   (setq ami-emacs-version
         (concat "gmacs-" (replace-regexp-in-string "\\.[0-9]+$" ""
                                                    emacs-version)))
   (setq debian-emacs-flavor (make-symbol ami-emacs-version))
   (add-to-list 'load-path (concat "/usr/share/" ami-emacs-version
-                                  "/site-lisp/google/"))
-  (require 'google))
+                                  "/site-lisp/emacs-google-config/"
+                                  "devtools/editors/emacs/"))
+  (require 'google)
+  (require 'google-ycmd))
 
 (provide 'setup-google)
 ;;; setup-google.el ends here
